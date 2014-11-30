f94e56d7c78cf9e80a91cb2c6d49d39dcbe54665
Eric Ren <eric.ren@me.com>
2014-11-30 14:18:04 -0500

    patch

--- a3/q4d/patched.php
+++ a3/q4d/patched.php
@@ -1,5 +1,6 @@
 <?php
 $username = addslashes($_POST['username']);  # username field from POST'ed HTML form
 $sql = "SELECT * FROM Person WHERE Username = '$username' ";
-$rs = $db->executeQuery($sql);
+$query = $db->prepare($sql);
+$rs = $db->executeQuery($query);
 ?>
